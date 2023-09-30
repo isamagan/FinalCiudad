3b77158760485d45bd6b75f566291c03ec0a8dbc
maganisa <120689923+maganisa@users.noreply.github.com>
2023-09-30 00:09:16 -0300

    estilo tabla admin

--- routes/contacto.js
+++ routes/contacto.js
@@ -5,7 +5,7 @@ var router=express.Router();
 router.get('/',function(req, res,next){
     res.render('index',{
         isContacto:true
-    });// es el final.hbs
+    });
 });
 
 module.exports=router;
